5fc67a17e1eec4f9b3210b99375a15966c5b0898
Luisa Arévalo <larevalo173@gmail.com>
2025-06-12 20:26:19 -0600

    chore: Adding docker to spring and flyway migration

---
+++ src/main/resources/db/migration/V2__insert_init_values.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.roles(name) VALUES ('ROLE_ADMIN');
+INSERT INTO public.roles(name) VALUES ('ROLE_USER');
+
+INSERT INTO public.departments(department_name) VALUES ('IT');
+INSERT INTO public.departments(department_name) VALUES ('HR');
+
+INSERT INTO employees(name, last_name, email, username, password, department_id) VALUES ('John', 'Doe', 'john@doe.com', 'johndoe', 'password123', 1);
+INSERT INTO employees(name, last_name, email, username, password, department_id) VALUES ('Jane', 'Smith', 'jane@smith.com', 'janesmith', 'password456', 2);
+
+INSERT INTO public.employee_roles(employee_id, role_id) VALUES (1, 1);
+INSERT INTO public.employee_roles(employee_id, role_id) VALUES (2, 2);
